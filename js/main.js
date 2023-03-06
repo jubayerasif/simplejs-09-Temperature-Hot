c250b6d4a0eb7ea03b6a00d7f8a74bdbca4c79cc
jubayerasif <jubaerasif101@gmail.com>
2023-03-06 13:10:19 +0600

    done

--- js/main.js
+++ js/main.js
@@ -1,49 +1,24 @@
 const API_KEY =`e3f1be6531515252232d131c764b7c15`;
-const searchTemperature = () =>{
-    const city = document.getElementById('city-name').value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+const searchTemperature = () => {
+        const city = document.getElementById('city-name').value;
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
 
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayTemperature(data));
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displayTemperature(data));
 }
-
-const setInnerText(id, text){
+const setInnerText = (id, text) => {
     document.getElementById(id).innerText = text;
 }
 
 const displayTemperature = temperature => {
     setInnerText('city', temperature.name);
-    setInnerText('temperature', temperature.main.temp);
+    setInnerText('country', temperature.main.temp);
     setInnerText('condition', temperature.weather[0].main);
+
     const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
     const imgIcon = document.getElementById('weather-icon');
     imgIcon.setAttribute('src', url);
 }
 
 
-
-
-// const API_KEY = `46ad7457603b9b0104e633e78cd60e16`;
-// const searchTemperature = () => {
-//     const city = document.getElementById('city-name').value;
-//     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
-
-//     fetch(url)
-//         .then(res => res.json())
-//         .then(data => displayTemperature(data));
-// }
-
-// const setInnerText = (id, text) => {
-//     document.getElementById(id).innerText = text;
-// }
-
-// const displayTemperature = temperature => {
-//     setInnerText('city', temperature.name);
-//     setInnerText('temperature', temperature.main.temp);
-//     setInnerText('condition', temperature.weather[0].main);
-//     // set weather icon
-//     const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
-//     const imgIcon = document.getElementById('weather-icon');
-//     imgIcon.setAttribute('src', url);
-// }
